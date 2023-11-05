6d450eb15870fa30fb33539979a92e46d1a7baa2
Sahil Sharma <fyrepyryte@gmail.com>
2023-11-05 14:23:06 -0700

    new file:   app/captstonewebsiteprototype.py 	renamed:    haar.py -> app/haar.py 	renamed:    haar_theoretical.py -> app/haar_theoretical.py 	new file:   app/static/Haar Theoretical Results.png 	new file:   app/static/Two-Design Theoretical Results.png 	new file:   app/templates/haar.html 	new file:   app/templates/twodesign.html 	renamed:    two_design.py -> app/two_design.py 	renamed:    two_design_theoretical.py -> app/two_design_theoretical.py 	deleted:    captstonewebsiteprototype.py 	deleted:    index.html 	modified:   pythonwebserviceproofofconcept.py

---
+++ app/captstonewebsiteprototype.py
@@ -0,0 +1,30 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def redirect_to_haar():
+    return redirect("/haar")
+
+
+@app.route('/haar')
+def haar():
+    # You can read the code snippet from a file (e.g., code.txt)
+    # and the image from a file (e.g., image.jpg) and pass them to the template.
+    with open("app\\haar.py", "r") as code_file:
+        code_snippet = code_file.read()
+    
+    return render_template(r'haar.html', code=code_snippet, image='Haar Theoretical Results')
+
+@app.route("/twodesign")
+def twoDesign():
+    # You can read the code snippet from a file (e.g., code.txt)
+    # and the image from a file (e.g., image.jpg) and pass them to the template.
+    with open("app\\two_design.py", "r") as code_file:
+        code_snippet = code_file.read()
+    
+    return render_template(r'twodesign.html', code=code_snippet, image='Two-Design Theoretical Results.png')
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- pythonwebserviceproofofconcept.py
+++ pythonwebserviceproofofconcept.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import *
 
 app = Flask(__name__)
 
