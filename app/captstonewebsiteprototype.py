e17dbea427033f98507e72d191c4f8abbcb9af70
Sahil <fyrepyryte@gmail.com>
2023-11-19 19:01:09 -0700

    modified:   app/captstonewebsiteprototype.py 	new file:   app/clifford.py 	new file:   app/clifford_theoretical.py 	new file:   app/templates/clifford.html 	modified:   app/templates/haar.html 	modified:   app/templates/twodesign.html

--- app/captstonewebsiteprototype.py
+++ app/captstonewebsiteprototype.py
@@ -24,6 +24,15 @@ def twoDesign():
         code_snippet = code_file.read()
     
     return render_template(r'twodesign.html', code=code_snippet, image='Two-Design Theoretical Results.png')
+
+@app.route("/clifford")
+def clifford():
+    # You can read the code snippet from a file (e.g., code.txt)
+    # and the image from a file (e.g., image.jpg) and pass them to the template.
+    with open("app\\clifford.py", "r") as code_file:
+        code_snippet = code_file.read()
+    
+    return render_template(r'clifford.html', code=code_snippet, image='Two-Design Theoretical Results.png')
     
 
 if __name__ == "__main__":
