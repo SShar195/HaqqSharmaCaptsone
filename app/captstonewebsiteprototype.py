97017cb5bb4d239a017a7291c248ab69d963a4f6
SShar195 <fyrepyryte@gmail.com>
2024-01-28 10:58:46 -0700

    modified:   app/captstonewebsiteprototype.py 	modified:   app/templates/clifford.html 	modified:   app/templates/haar.html 	new file:   app/templates/pooledpauli.html 	modified:   app/templates/twodesign.html

--- app/captstonewebsiteprototype.py
+++ app/captstonewebsiteprototype.py
@@ -9,31 +9,33 @@ def redirect_to_haar():
 
 @app.route('/haar')
 def haar():
-    # You can read the code snippet from a file (e.g., code.txt)
-    # and the image from a file (e.g., image.jpg) and pass them to the template.
-    with open("app\\haar.py", "r") as code_file:
+    with open("app/haar.py", "r") as code_file:
         code_snippet = code_file.read()
     
     return render_template(r'haar.html', code=code_snippet, image='Haar Theoretical Results')
 
 @app.route("/twodesign")
 def twoDesign():
-    # You can read the code snippet from a file (e.g., code.txt)
-    # and the image from a file (e.g., image.jpg) and pass them to the template.
-    with open("app\\two_design.py", "r") as code_file:
+    with open("app/two_design.py", "r") as code_file:
         code_snippet = code_file.read()
     
     return render_template(r'twodesign.html', code=code_snippet, image='Two-Design Theoretical Results.png')
 
 @app.route("/clifford")
 def clifford():
-    # You can read the code snippet from a file (e.g., code.txt)
-    # and the image from a file (e.g., image.jpg) and pass them to the template.
-    with open("app\\clifford.py", "r") as code_file:
+    with open("app/clifford.py", "r") as code_file:
         code_snippet = code_file.read()
     
     return render_template(r'clifford.html', code=code_snippet, image='Two-Design Theoretical Results.png')
     
+
+@app.route("/pooledpauli")
+def pooledpauli():
+    with open("app/Pooled Pauli.py", "r") as code_file:
+        code_snippet = code_file.read()
+    
+    return render_template(r'pooledpauli.html', code=code_snippet, image='Two-Design Theoretical Results.png')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
